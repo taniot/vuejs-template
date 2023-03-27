4694342aa0e20bcffcdf7f74cef5273bd7678e27
taniot <gaetano.frascolla@gmail.com>
2023-03-27 10:16:02 +0200

    feat: starting scaffolding

---
+++ src/components/HelloApp.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default {
+    name: 'Hello'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Hello Classe 89</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
